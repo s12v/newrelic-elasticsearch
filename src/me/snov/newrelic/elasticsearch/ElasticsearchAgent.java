ea1642777a907de841909e57cc0698372c62f647
Sergey Novikov <snov@snov.me>
2015-02-15 17:15:07 +0100

    Release 1.0.0

--- src/me/snov/newrelic/elasticsearch/ElasticsearchAgent.java
+++ src/me/snov/newrelic/elasticsearch/ElasticsearchAgent.java
@@ -18,7 +18,7 @@ import java.util.Map;
 public class ElasticsearchAgent extends Agent {
 
     private static final String GUID = "me.snov.newrelic-elasticsearch";
-    private static final String VERSION = "0.9.0";
+    private static final String VERSION = "1.0.0";
 
     private final String clusterName;
     private final ClusterStatsParser clusterStatsParser;
