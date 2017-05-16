1dc1cb94d7d83f4c8ace5a652d55e71dbd0d532a
gdmelloatpoints <gavin.dmello@points.com>
2017-05-16 16:19:05 -0400

    removed comments.

--- src/main/java/me/snov/newrelic/elasticsearch/responses/ClusterHealth.java
+++ src/main/java/me/snov/newrelic/elasticsearch/responses/ClusterHealth.java
@@ -3,18 +3,18 @@ package me.snov.newrelic.elasticsearch.responses;
 import java.util.ArrayList;
 
 public class ClusterHealth {
-    public String status; //
-    public Number number_of_nodes; //
+    public String status; 
+    public Number number_of_nodes; 
     public Boolean timed_out;
-    public Number number_of_data_nodes; //
-    public Number active_primary_shards; //
-    public Number active_shards; //
-    public Number relocating_shards;//
-    public Number initializing_shards; //
-    public Number unassigned_shards; //
-    public Number delayed_unassigned_shards; //
-    public Number number_of_pending_tasks; //
+    public Number number_of_data_nodes; 
+    public Number active_primary_shards; 
+    public Number active_shards; 
+    public Number relocating_shards;
+    public Number initializing_shards; 
+    public Number unassigned_shards; 
+    public Number delayed_unassigned_shards; 
+    public Number number_of_pending_tasks; 
     public Number number_of_in_flight_fetch;
-    public Number task_max_waiting_in_queue_millis; //
-    public Number active_shards_percent_as_number; //
+    public Number task_max_waiting_in_queue_millis; 
+    public Number active_shards_percent_as_number; 
 }
